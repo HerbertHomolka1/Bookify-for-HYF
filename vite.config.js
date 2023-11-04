a38c0b8b61d01f6c265f4f8d249d2dab015ddaa4
HerbertHomolka1 <herberthomolka0@gmail.com>
2023-11-04 12:34:22 +0100

    changed build and base in vte.confi.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "http://HerbertHomolka1.github.io/bookify-for-hyf",
+  base: "/bookify-for-hyf",
+  build: {
+     chunkSizeWarningLimit: 1600,
+   }
 })
